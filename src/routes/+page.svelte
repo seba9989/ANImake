e62d783d67e26c467e2aea2f2d85383d822e6f30
Seba9989 <sgrzybbowski@gmail.com>
2025-12-12 13:26:03 +0100

    Auth Fix

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,4 @@
 <script lang="ts">
-	import { authClient } from '$lib/auth-client';
-	import { onMount } from 'svelte';
-
-	onMount(async () => {
-		const { data, error } = await authClient.signUp.email({
-			email: 'sgrzybbowski@gmail.com',
-			name: 'Seba9989',
-			password: 'budrys12',
-		});
-		console.log(data);
-		console.log(error);
-	});
 </script>
 
 <h1>Welcome to SvelteKit</h1>
